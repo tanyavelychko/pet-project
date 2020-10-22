7e7785dda197124aadcd8871dde4f68ec7d26819
tanyavelychko <tanya.velychko@gmail.com>
2020-10-22 22:37:43 +0300

    init client side application

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Auth from './components/Auth';
+import Registration from './components/Registration/Registration';
+import Calculator from './components/Calculator/Calculator';
+import Header from './components/Header/Header';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Redirect from='/' to='/calculator' />
+            <Switch>
+                <Route path='/auth'>
+                    <Auth />
+                </Route>
+                <Route path='/registration'>
+                    <Registration />
+                </Route>
+                <Route path='/calculator'>
+                    <Calculator />
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
